d923b78810d4943129636996b2e818673aa4ab86
charlie <charlie@anglo.is>
2023-06-21 23:28:50 +0100

    renamed main.py to example.py

--- verify/example.py
+++ verify/example.py
@@ -4,6 +4,8 @@ from verify import Verify, Classifier
 screenshots = os.listdir('screenshots')
 
 if __name__ == '__main__':
+    print('Verifying screenshots...')
     for file in screenshots:
         output = Verify('./screenshots/' + file).verify_screenshot()
         Classifier(output, file).classify()
+    print('Done!')
